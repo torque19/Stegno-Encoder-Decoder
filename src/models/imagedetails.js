9bb0840afa6ffceb529b8d34c0c37cabc4a5e0d3
Tushar Jha <46811756+torque19@users.noreply.github.com>
2021-09-10 21:45:47 +0530

    Delete src directory

--- src/models/imagedetails.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const imageInputSchema = new mongoose.Schema({
-        data:Buffer
-        
-});
-
-const Image = new mongoose.model('Image', imageInputSchema);
-module.exports = Image;
